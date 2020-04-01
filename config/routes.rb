ca3914cd88f7c98dd239e57f6d031d811a8f6ea6
chirs <chrispod47@gmail.com>
2020-04-01 18:31:45 -0500

    added index show controller for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
   get 'categories/index'
   get 'categories/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
