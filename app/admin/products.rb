fc56b8fbb748cc941c9f2503fa45cea9df3932c5
chirs <chrispod47@gmail.com>
2020-04-01 19:19:15 -0500

    pre merge commit

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -7,6 +7,14 @@ ActiveAdmin.register Product do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :category_id, :name, :description, :cost_cents, :product_status, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
   #
   # or
   #
