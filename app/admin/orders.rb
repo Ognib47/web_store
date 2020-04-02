f7fda7dee491ede9e9b5901045954253e8e9f1d1
chirs <chrispod47@gmail.com>
2020-04-02 15:11:46 -0500

    just removed some unnescesasry text from app/admi

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,18 +1,5 @@
-ActiveAdmin.register Order do
+# frozen_string_literal: true
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :gst_rate, :pst_rate, :total_cost, :users_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:gst_rate, :pst_rate, :total_cost, :users_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+ActiveAdmin.register Order do
+  permit_params :gst_rate, :pst_rate, :total_cost, :users_id
 end
--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Product do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
   permit_params :category_id, :name, :description, :cost_cents, :product_status, :image
 
   form do |f|
