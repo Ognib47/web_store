3cc7643a1a6f25f0be991278c253df0cbf0cca06
chirs <chrispod47@gmail.com>
2020-04-02 14:08:48 -0500

    attempted to fix actice admin still not 100%

--- app/admin/provinces.rb
+++ app/admin/provinces.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-ActiveAdmin.register Order do
+ActiveAdmin.register Province do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :users_id, :gst_rate, :pst_rate, :total_cost
+  permit_params :province
   #
   # or
   #
   # permit_params do
-  #   permitted = [:customer_id, :gst_rate, :pst_rate, :total_cost]
+  #   permitted = [:province]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
--- db/migrate/20200402184739_add_reference_to_users.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddReferenceToUsers < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null :users, :provinces_id, false
-  end
-end
