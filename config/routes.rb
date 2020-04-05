ecf5e172feefd742be002fab7299a28aa503e283
chirs <chrispod47@gmail.com>
2020-04-05 16:26:56 -0500

    added stripe config and madea checkout controller, as well as routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
   delete 'products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart'
   resources 'categories', only: %i[index show]
 
+  scope 'checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+  end
   # get 'categories/index'
   # get 'categories/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
