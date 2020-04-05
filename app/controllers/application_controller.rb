b6fbaa267e270fc35b020b947946e8043bbd5136
chirs <chrispod47@gmail.com>
2020-04-04 19:18:27 -0500

    added some navigation and a products partial. Needs to be restyled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :province_id, :password) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
-  end
 end
